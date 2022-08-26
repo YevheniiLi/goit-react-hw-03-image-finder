8e0c926f9dc1307c7e92df1905a618847cde5281
Yevhenii Lymarenko <jekilllimarenko@gmail.com>
2022-08-26 16:21:23 +0300

    add modal

--- src/components/App.js
+++ src/components/App.js
@@ -4,37 +4,40 @@ import Modal from '../components/Modal';
 export class App extends Component {
 
     state ={
-        showModal: false
+        showModal: false,
     }
 
 
-
+    // Создание метода по открытию и закрытию модалки 
         toggleModal = () =>{
             this.setState(({showModal}) =>({
-                showModal: !showModal
-            }))
-        }
-
-
-
-
-
-
-
-
+                showModal: !showModal,
+            }));
+        };
 
   render() {
 
     const {showModal} = this.state;
 
+
+    // Логический оператор AND ( && ) true в противном случае false
     return (
       <>
-      <button type='button'onClick={this.toggleModal}>Открыть модалку</button>
-      {showModal && <Modal>
+      <button type='button'onClick={this.toggleModal}>
+        Открыть модалку
+        </button>
+    
+        
+      {showModal && (
+      <Modal onClose={this.toggleModal} > 
         <h1>Privet ya modalka</h1>
         <p>qweqweqwe</p>
-        <button type='button' onClick={this.toggleModal}>Закрыть модалку</button>
-      </Modal> }
+        <button type='button' onClick={this.toggleModal}>
+          Закрыть модалку
+          </button>
+      </Modal> 
+      
+      )}
         
       </>
     );
